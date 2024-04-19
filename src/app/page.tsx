ae10aadfaf37e98d3b34f82a4f0630e90d07737d
Mustafa Azad <mustafaazad533@gmail.com>
2024-04-19 20:06:24 +0530

    Server side pagination functionality added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import reportData from '../data/reports.json';
 import Table from "@/components/ReportList/Table";
+import Pagination from '@/components/utils/Pagination';
 import Image from "next/image";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
   const reports = reportData.reports;
-  
+  const page = parseInt(Array.isArray(searchParams['page']) ? '1' : searchParams['page'] || '1', 10); // Parse page as integer, default to 1
+  const per_page = parseInt(Array.isArray(searchParams['per_page']) ? '5' : searchParams['per_page'] || '5', 10); // Parse per_page as integer, default to 5
+
+  // Calculate start and end indices based on pagination
+  const start = (page - 1) * per_page;
+  const end = Math.min(start + per_page, reports.length);
+
+  const perPageReports = reports.slice(start, end);
   return (
     <div className="bg-background-primary h-screen w-screen flex justify-center items-center">
       <div className='h-3/4 w-2/4 bg-white rounded-xl'>
@@ -20,7 +32,8 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <Table entities={reports} />
+      <Table entities={perPageReports} />
+      <Pagination hasNextPage={reports?.length > end} hasPrevPage={start > 0} />
     </div>
     </div>
   );
--- src/components/utils/Pagination.tsx
+++ src/components/utils/Pagination.tsx
@@ -1,11 +1,68 @@
-import React from 'react'
+'use client'
 
-const Pagination = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React, { FC, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+interface PaginationControlsProps {
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
 }
 
-export default Pagination
+const Pagination: FC<PaginationControlsProps> = ({
+  hasNextPage,
+  hasPrevPage,
+}) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const page = searchParams.get('page') ?? '1';
+  const [per_page, setPerPage] = useState(searchParams.get('per_page') ?? '5');
+
+  const handlePerPageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newPerPage = event.target.value;
+    setPerPage(newPerPage);
+    router.push(`/?page=${page}&per_page=${newPerPage}`);
+  };
+
+  return (
+    <div className='flex gap-2'>
+      <button
+        className='bg-blue-500 text-white p-1'
+        disabled={!hasPrevPage}
+        onClick={() => {
+          router.push(`/?page=${Number(page) - 1}&per_page=${per_page}`);
+        }}
+      >
+        prev page
+      </button>
+
+      <div>
+        {page} / {Math.ceil(25 / Number(per_page))}
+      </div>
+
+      {/* Select dropdown for changing rows per page */}
+      <select
+        value={per_page}
+        onChange={handlePerPageChange}
+        className='p-1 border border-gray-300 rounded'
+      >
+        <option value='5'>5</option>
+        <option value='10'>10</option>
+        <option value='20'>20</option>
+        <option value='25'>25</option>
+      </select>
+
+      <button
+        className='bg-blue-500 text-white p-1'
+        disabled={!hasNextPage}
+        onClick={() => {
+          router.push(`/?page=${Number(page) + 1}&per_page=${per_page}`);
+        }}
+      >
+        next page
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
